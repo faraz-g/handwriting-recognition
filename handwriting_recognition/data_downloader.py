de9056fffbd31cc9f40650b833b8e59b99b32757
Faraz Ghasemzadeh <faraz.g2000@gmail.com>
2024-04-06 19:02:10 +1000

    Pre-processing skeleton completed

--- handwriting_recognition/data_downloader.py
+++ handwriting_recognition/data_downloader.py
@@ -1,18 +1,86 @@
 import argparse
+import os
 import shutil
 import sys
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+from functools import partial
 
 import kaggle
+import pandas as pd
+from PIL import Image
+from tqdm import tqdm
 
 from handwriting_recognition.utils import get_dataset_folder_path
 
-# from multiprocessing import Pool
+
+def filter_missing_rows(labels: pd.DataFrame, dataset_folder: os.PathLike) -> pd.DataFrame:
+    def _return_path_if_file_missing(file: str, directory: os.PathLike) -> str:
+        if not os.path.exists(os.path.join(directory, file)):
+            return file
+        else:
+            return None
+
+    # Remove rows where there is no label
+    labels = labels[~labels["IDENTITY"].isna()]
+
+    # Remove rows where the filename is missing
+    partial_func = partial(_return_path_if_file_missing, directory=dataset_folder)
+
+    missing_files = set()
+    file_paths = labels["FILENAME"].to_list()
+    with ThreadPoolExecutor(2 * os.cpu_count()) as p:
+        with tqdm(total=len(file_paths)) as pbar:
+            for v in p.map(partial_func, file_paths, chunksize=4):
+                pbar.update()
+                missing_files.add(v)
+
+    labels = labels[~labels["FILENAME"].isin(missing_files)]
+
+    return labels
+
+
+def process_single_image(image_path: os.PathLike, target_folder: os.PathLike) -> None:
+    image = Image.open(image_path)
+
+    # TODO add processing here
+
+    image.save(os.path.join(target_folder, os.path.basename(image_path)))
+
+
+def process_images(image_folder: os.PathLike, target_folder: os.PathLike) -> None:
+    os.makedirs(target_folder)
+    all_files = [os.path.join(image_folder, file_path) for file_path in os.listdir(image_folder)]
+    partial_func = partial(process_single_image, target_folder=target_folder)
+
+    with ProcessPoolExecutor(2 * os.cpu_count()) as p:
+        with tqdm(total=len(all_files)) as pbar:
+            for _ in p.map(partial_func, all_files, chunksize=4):
+                pbar.update()
 
 
 def pre_process() -> None:
-    pass
-    # dataset_folder = get_dataset_folder_path()
-    # processed_dataset_dir = dataset_folder.joinpath("processed")
+    dataset_folder = get_dataset_folder_path()
+    pre_processed_folder = dataset_folder / "pre_processed"
+    try:
+        shutil.rmtree(pre_processed_folder)
+    except FileNotFoundError:
+        pass
+
+    pre_processed_folder.mkdir()
+    for dataset in ["train", "test", "validation"]:
+        labels_file_name = f"{dataset}.csv"
+        image_folder = dataset_folder / "raw" / dataset
+        target_image_folder = pre_processed_folder / dataset
+
+        process_images(image_folder=image_folder, target_folder=target_image_folder)
+
+        labels = pd.read_csv(dataset_folder / "raw" / labels_file_name)
+        labels = filter_missing_rows(labels=labels, dataset_folder=target_image_folder)
+        labels = labels.rename(columns={"FILENAME": "file_name", "IDENTITY": "label"})
+        labels["file_path"] = labels["file_name"].apply(lambda x: os.path.join(target_image_folder, x))
+        labels["label"] = labels["label"].apply(lambda x: x.lower())
+
+        labels.to_csv(pre_processed_folder / labels_file_name, index=None)
 
 
 def download_data_from_kaggle() -> None:
@@ -27,8 +95,12 @@ def download_data_from_kaggle() -> None:
     if not confirmation.lower() == "y":
         sys.exit(0)
 
-    output_dataset_dir.mkdir(parents=True, exist_ok=True)
-    shutil.rmtree(output_dataset_dir)
+    try:
+        shutil.rmtree(output_dataset_dir)
+    except FileNotFoundError:
+        pass
+
+    output_dataset_dir.mkdir(parents=True)
 
     dataset_reference = kaggle.api.dataset_list(search="handwriting-recognition", user="landlord")
     if not len(dataset_reference) == 1:
@@ -63,5 +135,7 @@ if __name__ == "__main__":
     if args.download:
         download_data_from_kaggle()
 
-    if args.process:
+    if args.pre_process:
         pre_process()
+
+    sys.exit()
--- handwriting_recognition/data_loader.py
+++ handwriting_recognition/data_loader.py
@@ -1,53 +1,42 @@
-# import os
-# from pathlib import Path
-# from typing import Any
+import os
+from typing import Any
 
-# import cv2
-# import pandas as pd
+import cv2
+import pandas as pd
+
 # import torch
-# from albumentations import Compose
+from albumentations import Compose
+
 # from albumentations.pytorch.transforms import ToTensorV2
 # from PIL import Image
-# from torch.utils.data import Dataset
+from torch.utils.data import Dataset
 
 
-# class HandWritingDataset(Dataset):
-#     def __init__(
-#         self,
-#         images_folder: str | Path,
-#         dataset_df: pd.DataFrame,
-#         augmentations: Compose | None,
-#     ) -> None:
-#         super().__init__()
-#         self.mode = mode
-#         df = pd.read_csv(os.path.join(data_folder_path, data_path))
-#         df = df.reset_index()
-#         if dataset is not None:
-#             df = df[df["dataset"] == dataset]
-#         self.images_df = df[["cropped_face_path", "label", "dataset"]]
-#         self.data_folder_path = data_folder_path
-#         self.augmentations = augmentations
+class HandWritingDataset(Dataset):
+    def __init__(
+        self,
+        data_path: os.PathLike,
+        augmentations: Compose | None,
+    ) -> None:
+        super().__init__()
+        df = pd.read_csv(data_path)
+        df = df.reset_index()
 
-#     def __getitem__(self, index: int) -> Any:
-#         image_data = self.images_df.iloc[index]
-#         cropped_face_path = image_data["cropped_face_path"]
-#         label = image_data["label"]
-#         image_path = os.path.join(self.data_folder_path, cropped_face_path)
+        self.df = df
+        self.augmentations = augmentations
 
-#         image = cv2.imread(image_path)
+    def __getitem__(self, index: int) -> Any:
+        image_data = self.images_df.iloc[index]
+        label = image_data["label"]
+        image_path = image_data["file_path"]
 
-#         if self.augmentations is not None:
-#             image = self.augmentations(image=image)["image"]
+        image = cv2.imread(image_path)
 
-#         # os.makedirs("test_outputs", exist_ok=True)
-#         # debug_out_path = os.path.join("test_outputs", f"{label}_{os.path.basename(cropped_face_path)}")
-#         # cv2.imwrite(debug_out_path, image)
+        # TODO Apply augmentations here
+        # TODO Convert label to tensor
+        # TODO Convert image to tensor
 
-#         image = img_to_tensor(image)
-#         label = 1 if label.lower() == "fake" else 0
-#         label = torch.tensor(label, dtype=torch.float)
+        return image, label
 
-#         return image, label
-
-#     def __len__(self) -> int:
-#         return len(self.images_df)
+    def __len__(self) -> int:
+        return len(self.images_df)
