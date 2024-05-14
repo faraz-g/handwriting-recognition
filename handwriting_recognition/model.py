de2f4c9bde742bcc44e6d7608794249f5e52c202
Matt Bowman <34254911+mattbowman2@users.noreply.github.com>
2024-05-15 00:52:45 +1000

    Added ViT model with pretrained weights

--- handwriting_recognition/model.py
+++ handwriting_recognition/model.py
@@ -5,8 +5,19 @@ from transformers import ViTModel
 # TODO
 class HandwritingRecognitionModel(torch.nn.Module):
     def __init__(self, image_feature_extractor: ViTModel):
+        super(HandwritingRecognitionModel, self).__init__()
         self.image_feature_extractor = image_feature_extractor
-        pass
 
-    def forward(self):
-        raise NotImplementedError
+    def forward(self, x, y=None):
+        self.last_hidden_states = self.image_feature_extractor(**x)
+
+        # TODO
+
+        return self.last_hidden_states  # This is temporary. DELTE ME
+
+    def predict(self, x):
+        self.last_hidden_states = self.image_feature_extractor(**x)
+
+        # TODO
+
+        return self.last_hidden_states  # This is temporary. DELTE ME
