0734dab9a5336f3ff870d31d08a3abbaace16031
Faraz Ghasemzadeh <faraz.g2000@gmail.com>
2024-05-21 18:43:03 +1000

    cleaned up changes

--- handwriting_recognition/model/model.py
+++ handwriting_recognition/model/model.py
@@ -4,6 +4,8 @@ from handwriting_recognition.utils import TrainingConfig
 from handwriting_recognition.model.bidirectional_lstm import BidirectionalLSTM
 from handwriting_recognition.model.attention import Attention
 
+LSTM_INPUT_SIZE_MAP = {"resnet34": 49, "vit_base_patch16_224.augreg2_in21k_ft_in1k": 768}
+
 
 class HandwritingRecognitionModel(torch.nn.Module):
     def __init__(self, image_feature_extractor: nn.Module, training_config: TrainingConfig):
@@ -13,11 +15,14 @@ class HandwritingRecognitionModel(torch.nn.Module):
 
         # Image Feature Extraction
         self.image_feature_extractor = image_feature_extractor
-        self.img_hidden_size = 512
 
         # Sequence Modelling
         self.sequence_modelling = nn.Sequential(
-            BidirectionalLSTM(49, self.training_config.lstm_hidden_size, self.training_config.lstm_hidden_size),
+            BidirectionalLSTM(
+                LSTM_INPUT_SIZE_MAP[training_config.feature_extractor_config.model_name],
+                self.training_config.lstm_hidden_size,
+                self.training_config.lstm_hidden_size,
+            ),
             BidirectionalLSTM(
                 self.training_config.lstm_hidden_size,
                 self.training_config.lstm_hidden_size,
@@ -35,7 +40,10 @@ class HandwritingRecognitionModel(torch.nn.Module):
 
     def forward(self, x, y, is_train):
         image_features = self.image_feature_extractor.forward_features(x)
-        image_features = image_features.view(image_features.shape[0], image_features.shape[1], -1)
+
+        if self.training_config.feature_extractor_config.model_name == "resnet34":
+            image_features = image_features.view(image_features.shape[0], image_features.shape[1], -1)
+
         lstm_features = self.sequence_modelling(image_features)
 
         prediction = self.prediction(
